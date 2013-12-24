9fb31edaa13538ae4f590c581ac160e10c621f0d
Gary M. Josack <gary@byoteki.com>
2013-12-24 14:57:58 -0800

    Implemented cli and some cleanups to models/client

--- tattrdb/__init__.py
+++ tattrdb/__init__.py
@@ -10,7 +10,7 @@ __author__ = "Gary M. Josack <gary@byoteki.com>"
 
 
 
-class TattrException(Exception):
+class Error(Exception):
     """Base level exception for the Tattr library."""
 
 
@@ -37,7 +37,7 @@ class Collection(object):
 
     def __init__(self):
         if self.model is None:
-            raise TattrException("Collection class is abstract only.")
+            raise Error("Collection class is abstract only.")
 
         self.db = None
 
@@ -64,13 +64,13 @@ class Hosts(Collection):
             self.db.commit()
         except IntegrityError:
             self.db.rollback()
-            raise TattrException("Hostname (%s) already exists." % name)
+            raise Error("Hostname (%s) already exists." % name)
 
     def rm(self, name):
         try:
             host = self.db.query(self.model).filter_by(hostname=name).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % name)
+            raise Error("Hostname (%s) doesn't exist." % name)
         self.db.delete(host)
         self.db.commit()
 
@@ -78,20 +78,20 @@ class Hosts(Collection):
         try:
             host = self.db.query(self.model).filter_by(hostname=old_name).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % old_name)
+            raise Error("Hostname (%s) doesn't exist." % old_name)
 
         try:
             host.hostname = new_name
             self.db.commit()
         except IntegrityError:
             self.db.rollback()
-            raise TattrException("Hostname (%s) already exists." % new_name)
+            raise Error("Hostname (%s) already exists." % new_name)
 
     def get(self, name):
         try:
             host = self.db.query(self.model).filter_by(hostname=name).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % name)
+            raise Error("Hostname (%s) doesn't exist." % name)
         return host.as_dict()
 
     def query(self, query):
@@ -101,11 +101,11 @@ class Hosts(Collection):
         try:
             host = self.db.query(self.model).filter_by(hostname=hostname).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % hostname)
+            raise Error("Hostname (%s) doesn't exist." % hostname)
         try:
             tag = self.db.query(models.Tag).filter_by(tagname=tagname).one()
         except NoResultFound:
-            raise TattrException("Tag (%s) doesn't exist." % tagname)
+            raise Error("Tag (%s) doesn't exist." % tagname)
 
         host.tags.append(tag)
         self.db.commit()
@@ -114,11 +114,11 @@ class Hosts(Collection):
         try:
             host = self.db.query(self.model).filter_by(hostname=hostname).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % hostname)
+            raise Error("Hostname (%s) doesn't exist." % hostname)
         try:
             tag = self.db.query(models.Tag).filter_by(tagname=tagname).one()
         except NoResultFound:
-            raise TattrException("Tag (%s) doesn't exist." % tagname)
+            raise Error("Tag (%s) doesn't exist." % tagname)
 
         try:
             host.tags.remove(tag)
@@ -130,12 +130,12 @@ class Hosts(Collection):
         try:
             host = self.db.query(self.model).filter_by(hostname=hostname).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % hostname)
+            raise Error("Hostname (%s) doesn't exist." % hostname)
 
         try:
             attr = self.db.query(models.Attribute).filter_by(attrname=attrname).one()
         except NoResultFound:
-            raise TattrException("Attribute (%s) doesn't exist." % attrname)
+            raise Error("Attribute (%s) doesn't exist." % attrname)
 
         try:
             assoc = (self.db.query(models.HostAttributes)
@@ -155,12 +155,12 @@ class Hosts(Collection):
         try:
             host = self.db.query(self.model).filter_by(hostname=hostname).one()
         except NoResultFound:
-            raise TattrException("Hostname (%s) doesn't exist." % hostname)
+            raise Error("Hostname (%s) doesn't exist." % hostname)
 
         try:
             attr = self.db.query(models.Attribute).filter_by(attrname=attrname).one()
         except NoResultFound:
-            raise TattrException("Attribute (%s) doesn't exist." % attrname)
+            raise Error("Attribute (%s) doesn't exist." % attrname)
 
         try:
             assoc = (self.db.query(models.HostAttributes)
@@ -189,15 +189,15 @@ class Tags(Collection):
             self.db.commit()
         except IntegrityError:
             self.db.rollback()
-            raise TattrException("Tag (%s) already exists." % name)
+            raise Error("Tag (%s) already exists." % name)
 
     def rm(self, name, force=False):
         try:
             tag = self.db.query(self.model).filter_by(tagname=name).one()
         except NoResultFound:
-            raise TattrException("Attribute (%s) doesn't exist." % name)
+            raise Error("Attribute (%s) doesn't exist." % name)
         if tag.hosts and not force:
-            raise TattrException("Tried to remove tag in use by %s hosts without force." % len(tag.hosts))
+            raise Error("Tried to remove tag in use by %s hosts without force." % len(tag.hosts))
         self.db.delete(tag)
         self.db.commit()
 
@@ -211,14 +211,17 @@ class Attributes(Collection):
             self.db.commit()
         except IntegrityError:
             self.db.rollback()
-            raise TattrException("Attribute (%s) already exists." % name)
+            raise Error("Attribute (%s) already exists." % name)
 
     def rm(self, name, force=False):
         try:
             attr = self.db.query(self.model).filter_by(attrname=name).one()
         except NoResultFound:
-            raise TattrException("Attribute (%s) doesn't exist." % name)
+            raise Error("Attribute (%s) doesn't exist." % name)
         if attr.hosts and not force:
-            raise TattrException("Tried to remove attribute in use by %s hosts without force." % len(attr.hosts))
+            raise Error("Tried to remove attribute in use by %s hosts without force." % len(attr.hosts))
+
+        for assoc in attr.host_assocs:
+            self.db.delete(assoc)
         self.db.delete(attr)
         self.db.commit()
